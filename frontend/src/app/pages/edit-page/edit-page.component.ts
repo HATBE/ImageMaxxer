7ccbd7a407e9e102a32f62f8dd8c6caf3b6cea96
HATBE <admin@aarongensetter.ch>
2025-08-05 15:44:41 +0200

    new ui

--- frontend/src/app/pages/edit-page/edit-page.component.ts
+++ frontend/src/app/pages/edit-page/edit-page.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedImageService } from '../../services/SharedImageService';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './edit-page.component.html',
-  styleUrl: './edit-page.component.css'
+  styleUrl: './edit-page.component.css',
 })
 export class EditPageComponent {
+  protected previewImage: string | null = null;
 
+  public constructor(private router: Router, private sharedImageService: SharedImageService) {}
+
+  public ngOnInit(): void {
+    const image = this.sharedImageService.getFile();
+
+    if (!image) {
+      this.router.navigate(['/upload']);
+    }
+
+    this.previewImage = image ? URL.createObjectURL(image) : null;
+  }
 }
